e15ea2efc17736e1c32fcf57febc90a5e7b761ac
Erick Perez <eperez@Ericks-MBP.fios-router.home>
2020-05-12 09:33:31 -0400

    Added Header Component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <nav className='app-header'>
+                <h1>Nav Section</h1>
+        </nav>
+    )
+}
+
+export default Header
