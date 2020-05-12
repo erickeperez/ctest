eed17d7e3e438527e86a592bdbe9add15b285eb4
Erick Perez <eperez@Ericks-MBP.fios-router.home>
2020-05-12 10:04:41 -0400

    Added Main Component to App

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main(){
+    return(
+        <main className="app-main">
+            <h1>This is the main section</h1>
+            <p>This is header section paragraph</p>
+        </main>
+    )
+}
+export default Main
